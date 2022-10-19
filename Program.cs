6522387c0082440311b37e46f7eebd81687ba3ea
JLamzon <jlamzon@yahoo.com>
2022-10-19 00:34:34 -0700

    added notes

--- Program.cs
+++ Program.cs
@@ -13,30 +13,33 @@ while (playAgain != "no")
 
     while (true)
     {
-        
+        //using long variable for user input
         long validNum = long.MaxValue;
 
         Console.WriteLine("Give number and I will tell you if its odd or even!");
+
         //this string will be what used to get user input
         userNumber = Console.ReadLine();
+
+        //checking if user input are numbers but parsing data
         isNumber = Int64.TryParse(userNumber, out validNum);
 
         if (isNumber == false)
         {
-            Console.WriteLine("Try again...");
+            Console.WriteLine("Too long or wrong input...\nTry again...\n");
         }
         else if (validNum % 2 == 0)
         {
-            Console.WriteLine("You chose a even number");
+            Console.WriteLine("You chose a even number\n");
             break;
         }
         else if (validNum % 2 == 1)
         {
-            Console.WriteLine("You chose a odd number");
+            Console.WriteLine("You chose a odd number\n");
             break;
         }
     }
-    Console.WriteLine("Try again? press any key or type no");
+    Console.WriteLine("Try again? press any key or type no...");
     playAgain = Console.ReadLine().ToLower();
 }
 Console.WriteLine("Thanks for playing!");
